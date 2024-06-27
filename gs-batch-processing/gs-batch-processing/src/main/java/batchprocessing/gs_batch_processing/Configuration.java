22972b2648ae94f29b51066dcffd86d5c4757e76
Ojo Olorunfemi Adewale <adewaleolorunfemi74@gmail.com>
2024-06-27 23:03:45 +0100

    working progress

--- gs-batch-processing/gs-batch-processing/src/main/java/batchprocessing/gs_batch_processing/Configuration.java
+++ gs-batch-processing/gs-batch-processing/src/main/java/batchprocessing/gs_batch_processing/Configuration.java
@@ -2,12 +2,18 @@ package batchprocessing.gs_batch_processing;
 
 import javax.sql.DataSource;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
@@ -33,6 +39,25 @@ public class Configuration {
 	    .sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
 	    .dataSource(dataSource)
 	    .beanMapped()
+	    .build();
+	}
+	
+	@Bean
+	public Job importUserJob(JobRepository jobRepository,Step step1, JobCompletionNotificationListener listener) {
+	  return new JobBuilder("importUserJob", jobRepository)
+	    .listener(listener)
+	    .start(step1)
+	    .build();
+	}
+
+	@Bean
+	public Step step1(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
+	          FlatFileItemReader<Person> reader, PersonItemProcessor processor, JdbcBatchItemWriter<Person> writer) {
+	  return new StepBuilder("step1", jobRepository)
+	    .<Person, Person> chunk(3, transactionManager)
+	    .reader(reader)
+	    .processor(processor)
+	    .writer(writer)
 	    .build();
 	}
 }
