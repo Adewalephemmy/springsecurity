2f17c429adc55b3bd784594c33dc326cfaa4e2ad
Ojo Olorunfemi Adewale <adewaleolorunfemi74@gmail.com>
2024-06-28 22:57:57 +0100

    finishig

--- gs-batch-processing/gs-batch-processing/src/main/java/batchprocessing/gs_batch_processing/GsBatchProcessingApplication.java
+++ gs-batch-processing/gs-batch-processing/src/main/java/batchprocessing/gs_batch_processing/GsBatchProcessingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GsBatchProcessingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GsBatchProcessingApplication.class, args);
+		System.exit(SpringApplication.exit(SpringApplication.run(GsBatchProcessingApplication.class, args)));
 	}
 
 }
