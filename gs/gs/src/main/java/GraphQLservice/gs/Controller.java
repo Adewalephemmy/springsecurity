43575d3b3cbccb672c70acc584dd5e80672cafaa
Ojo Olorunfemi Adewale <adewaleolorunfemi74@gmail.com>
2024-06-30 09:04:28 +0100

    Controller

---
+++ gs/gs/src/main/java/GraphQLservice/gs/Controller.java
@@ -0,0 +1,19 @@
+package GraphQLservice.gs;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+	
+	@QueryMapping
+	public Book bookById(@Argument String id) {
+		return Book.getById(id);
+	}
+	
+	@SchemaMapping
+	public Author author(Book book) {
+		return Author.getById(book.authorId());
+	}
+}
