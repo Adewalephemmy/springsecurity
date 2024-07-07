378cb9cbf3e36cff34a19dd1d4fb4067c64da662
Ojo Olorunfemi Adewale <adewaleolorunfemi74@gmail.com>
2024-07-07 16:32:20 +0100

    classes

--- Git Practice/uploadingfile/uploadingfile/src/main/java/spring_upload/uploadingfile/UploadingfileApplication.java
+++ Git Practice/uploadingfile/uploadingfile/src/main/java/spring_upload/uploadingfile/UploadingfileApplication.java
@@ -1,7 +1,11 @@
 package spring_upload.uploadingfile;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import storage.StorageService;
 
 @SpringBootApplication
 public class UploadingfileApplication {
@@ -9,5 +13,12 @@ public class UploadingfileApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UploadingfileApplication.class, args);
 	}
-
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
