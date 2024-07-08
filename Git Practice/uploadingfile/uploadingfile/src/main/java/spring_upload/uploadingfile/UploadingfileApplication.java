1a04a20756d99be47b540d6461a7139af5a36a98
Ojo Olorunfemi Adewale <adewaleolorunfemi74@gmail.com>
2024-07-08 15:14:28 +0100

    finished

--- Git Practice/uploadingfile/uploadingfile/src/main/java/spring_upload/uploadingfile/UploadingfileApplication.java
+++ Git Practice/uploadingfile/uploadingfile/src/main/java/spring_upload/uploadingfile/UploadingfileApplication.java
@@ -3,11 +3,16 @@ package spring_upload.uploadingfile;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+import storage.StorageProperties;
 import storage.StorageService;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"spring_upload.uploadingfile", "storage"})
+@EnableConfigurationProperties(StorageProperties.class)
 public class UploadingfileApplication {
 
 	public static void main(String[] args) {
